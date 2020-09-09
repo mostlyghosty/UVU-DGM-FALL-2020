8122aa2c6527a7b23f03faa1403b4f1eef7bdf43
mostlyghostly <alyssapeterson300@gmail.com>
2020-09-09 17:17:49 -0600

    Created PlayerController, Moves vehicle forward

---
+++ GameCreate/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 20; //integers are private by default you have to declare it as public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the vehicle forwards
+        //transform.Translate(0, 0, 5); x is left and right, y is up and down, z is move forward or backwards in space
+        // transform.Translate(Vector3.forward); a command to move the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
