48ac21b807f0a334166aabde8082232b57921c30
mostlyghostly <alyssapeterson300@gmail.com>
2020-09-17 11:28:43 -0600

    Completed Plane Challenge 1

---
+++ Plane Program/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    //sets target for camera to follow
+    public GameObject player;
+
+    //defines offset of camera
+    private Vector3 offset = new Vector3(20, 0, 0);
+
+    //defines rotation of camera
+    private Vector3 rotate = new Vector3(0, 90, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets camera rotation before game starts
+        transform.Rotate(rotate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tracks camera to player
+        transform.position = player.transform.position + offset;
+       
+    }
+}
---
+++ Plane Program/Assets/Challenge 1/Scripts/MovePropellor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePropellor : MonoBehaviour
+
+{
+    //Set float for speed of propellor rotation
+    private float speed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate propellor
+        transform.Rotate(Vector3.forward * speed);
+    }
+}
