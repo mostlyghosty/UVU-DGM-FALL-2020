992c1db0e4385ebd8f6d34c234943fa8fd05c60d
mostlyghostly <alyssapeterson300@gmail.com>
2020-09-23 17:03:25 -0600

    Set Character to move

--- Feed the Animals/Assets/Scripts/PlayerController.cs
+++ Feed the Animals/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");  
+        horizontalInput = Input.GetAxis("Horizontal"); 
+
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * horizontalInput);
+
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
+        else if(transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
     }
 }
