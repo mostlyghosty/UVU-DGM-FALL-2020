99ae89a82fd6f47be803a501d0fdba4d7ae61726
mostlyghostly <alyssapeterson300@gmail.com>
2020-09-26 08:30:28 -0600

    Added set pieces, Camera restricted to set

--- Personal Project/Assets/Scripts/FollowPlayer.cs
+++ Personal Project/Assets/Scripts/FollowPlayer.cs
@@ -8,23 +8,28 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
 
     //defines offset of camera
-    public Vector3 offset = new Vector3(0, 0, 0);
+    public Vector3 offset = new Vector3(-20, 5, 0);
 
-    //defines rotation of camera
-    public Vector3 rotate = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        // Sets camera rotation before game starts
-        transform.Rotate(rotate);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //tracks camera to player
-        transform.position = player.transform.position + offset;
-       
+        if (player.transform.position.x < -25)
+        {
+           transform.position = new Vector3(-45, (player.transform.position.y + 5), player.transform.position.z);
+        }
+
+        else
+        {
+            transform.position = player.transform.position + offset;
+        }
+        
+
     }
 }
