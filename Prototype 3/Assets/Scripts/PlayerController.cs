616eb1bd26d2c6b95f5a0d117dcbee1833963fc8
mostlyghostly <alyssapeterson300@gmail.com>
2020-10-12 17:14:11 -0600

    Start Prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce;
+    public float gravityMod;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Stores the rigidbody component in the variable playerRB
+        playerRB = GetComponent<Rigidbody>();
+        // *= is a = a * b (keeps the value in a without replacing it)
+        Physics.gravity *= gravityMod;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //adds momentum in a direction (posititve y)
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
