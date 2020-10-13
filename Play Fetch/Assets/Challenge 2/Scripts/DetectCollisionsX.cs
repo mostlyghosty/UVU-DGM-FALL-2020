02db47a5c6dca46a93cfa75cb1cdbd35264a3a40
mostlyghostly <alyssapeterson300@gmail.com>
2020-10-13 11:52:16 -0600

    Finished Challenge 2

---
+++ Play Fetch/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    //detects collision between ball and dog and destroys only the ball
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float time;
+    private float timer;
+    
+    void Start()
+    {
+        //initializes variables
+        time = 0.25f;
+        timer = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets the timer based on time not frames and counts down
+        timer -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(timer < 0)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                //resets timer
+                timer = time;
+            }
+        }
+    }
+}
