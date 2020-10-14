696fa410ea87da3d0f103d993128987f9058bdca
mostlyghostly <alyssapeterson300@gmail.com>
2020-10-13 20:27:50 -0600

    Pick up object and open and close inventory

---
+++ Personal Project/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+   private GameObject puzzlePiece;
+   public string item;
+
+   
+    void Update()
+    {
+        if(puzzlePiece != null && Input.GetKeyDown(KeyCode.E))
+        {
+            
+            Debug.Log(item);
+            Destroy(puzzlePiece);
+            
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    
+            puzzlePiece = other.gameObject;
+            item = other.gameObject.name;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        item = null;
+        puzzlePiece = null;
+    }
+
+
+}
---
+++ Personal Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private GameObject [] slot;
+
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        // makes sure inventory isn't active and initializes time scale
+        inventory.SetActive(false);
+        Time.timeScale = 1;
+
+        //sets the number of slots in the allSlots array
+        allSlots = 9;
+        slot = new GameObject[allSlots];
+    
+    }
+    void Update()
+    {
+        //if left shift is pressed activate or deactivate inventory ui
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            inventoryEnabled = !inventoryEnabled;
+
+            if (inventoryEnabled == true)
+            {
+                Time.timeScale = 0;
+                inventory.SetActive(true);
+            }
+
+            else 
+            {
+                Time.timeScale = 1;
+                inventory.SetActive(false);
+            }
+
+        }
+    }
+}
