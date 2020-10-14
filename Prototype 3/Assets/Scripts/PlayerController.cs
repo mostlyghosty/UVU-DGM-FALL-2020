1b548f8b68bbcb1ea6efc878e767b1d1d2f8153d
mostlyghostly <alyssapeterson300@gmail.com>
2020-10-14 17:09:48 -0600

    Set up prefab and Spawn Manager

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,15 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityMod;
 
+    public bool isGrounded = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Stores the rigidbody component in the variable playerRB
         playerRB = GetComponent<Rigidbody>();
+
         // *= is a = a * b (keeps the value in a without replacing it)
         Physics.gravity *= gravityMod;
         
@@ -22,10 +25,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // == is a comparison
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             //adds momentum in a direction (posititve y)
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
     }
 }
---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3 (25, 0, 0);
+    
+    private float startDelay;
+
+    private float repeatInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //repeats spawn obstacles at a set variable
+      InvokeRepeating("SpawnObstacles", startDelay, repeatInterval);
+    }
+
+    // creates an instance of your obstacle
+    void SpawnObstacles()
+    {
+         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}   
