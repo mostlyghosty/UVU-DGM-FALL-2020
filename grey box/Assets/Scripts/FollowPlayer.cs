47e1bcf6ad0f4c85919d9e6af42bc2520043fd1a
mostlyghostly <alyssapeterson300@gmail.com>
2020-10-28 19:33:28 -0600

    Lab 3: added movement script to my player and set up camera

---
+++ grey box/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+     //sets target for camera to follow
+    public GameObject player;
+
+    //defines offset of camera
+    public Vector3 offset = new Vector3(2, 0, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player moves in a way that moves the camera out of bounds, put it back in bounds
+        if (player.transform.position.x > 3.5)
+        {
+           transform.position = new Vector3(5, player.transform.position.y, player.transform.position.z);
+        }
+
+        //if the camera is inbounds follows the player at a set distance
+        else
+        {
+            transform.position = player.transform.position + offset;
+        }
+        
+
+    }
+}
---
+++ grey box/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float forwardInput;
+    public int speed;
+    public float turnSpeed;
+    
+    public float playerRotation;
+
+    public float targetAngle;
+
+    public float diffAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets player rotation and puts it in terms of 180 degrees
+        playerRotation = transform.rotation.eulerAngles.y;
+
+        //calls horizontal and forward input from button press
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // If a direction button is pressed (if either input is not 0)
+        if (forwardInput != 0 || horizontalInput != 0)
+        {
+            //find the target angle based on horizontalInput and ForwardInput and convert from radians to degrees (atan2 is in (y,x))
+            targetAngle = Mathf.Atan2(-forwardInput, horizontalInput) * 180 / Mathf.PI + 180;
+
+            //takes the Difference between the target angle and the player rotation with mod 360 (makes sure the answer is always between 0 & 360 (and not negative), takes full revolutions out)
+            diffAngle = Mathf.Min((targetAngle - playerRotation + 360) % 360, (playerRotation - targetAngle + 360) % 360);
+
+
+            //If Target angle and Player rotation are different rotate until they are close
+            if (diffAngle > 10)
+            {
+               if (((targetAngle - playerRotation + 360) % 360) < ((playerRotation - targetAngle +360) % 360))
+               {
+                   transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+               }
+
+               else 
+               {
+                   transform.Rotate(Vector3.down * Time.deltaTime * turnSpeed);
+               }
+            }
+
+            // else go forward
+            else 
+            {
+                //Make rotation of character exact
+                transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+
+                transform.Translate(Vector3.back * Time.deltaTime * speed);
+            }
+
+        }
+
+    
+    }
+}
