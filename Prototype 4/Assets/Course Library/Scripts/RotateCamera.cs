241cdc38ab02acc09127b34f8ec62e82ed07d780
mostlyghostly <alyssapeterson300@gmail.com>
2020-11-04 17:18:54 -0700

    Prototype 4: Set up Camera, Player controller and half enemy AI

---
+++ Prototype 4/Assets/Course Library/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the Horizontal input for camera rotation around the z-axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //rotates camera around the z-axis
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
