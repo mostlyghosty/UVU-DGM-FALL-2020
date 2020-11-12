15510032521f7effb8860188c3f8520f4d1a3aa2
mostlyghostly <alyssapeterson300@gmail.com>
2020-11-11 17:22:20 -0700

    Prototype 4: Power Ups added

--- Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
 
     public GameObject focalPoint;
+    
+    public bool hasPowerUp;
+
+    private float powerUpStr = 15.0f;
+
+    public GameObject powerUpIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +30,37 @@ public class PlayerController : MonoBehaviour
     {
        float forwardInput = Input.GetAxis("Vertical");
        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+       powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDown());
+            powerUpIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 pushAway = (other.gameObject.transform.position - transform.position);
+            enemyRb.AddForce(pushAway * powerUpStr, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(7); hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+
     }
 }
+
