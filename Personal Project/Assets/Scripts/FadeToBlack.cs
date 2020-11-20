02a9cc370d1d4a4e608715232d75142f8bd41121
mostlyghostly <alyssapeterson300@gmail.com>
2020-11-20 10:30:03 -0700

    Personal Project: The end fade in not working

--- Personal Project/Assets/Scripts/FadeToBlack.cs
+++ Personal Project/Assets/Scripts/FadeToBlack.cs
@@ -8,22 +8,32 @@ public class FadeToBlack : MonoBehaviour
     public GameObject blackSquare;
     public bool endGame;
 
+    public GameObject theEnd;
+    private bool fadeIn = false;
+
     // Update is called once per frame
     void Update()
     {
         if (endGame)
         {
-            StartCoroutine(FadeOut());
+            StartCoroutine(Fade());
+        }
+
+        if (fadeIn)
+        {
+            StartCoroutine(Fade(false, 1));
         }
     }
 
-    public IEnumerator FadeOut(bool fadeOut = true, int fadeSpeed = 3)
+    public IEnumerator Fade(bool fadeOut = true, int fadeSpeed = 3)
     {
         //get the color of the square
         Color objectColor = blackSquare.GetComponent<Image>().color;
 
         //used to calculate how much black squares opacity will change each frame 
         float fadeAmount;
+
+        
 
         if(fadeOut)
         {
@@ -42,9 +52,30 @@ public class FadeToBlack : MonoBehaviour
                 //halts everything until the while loop ends
                 yield return null;
             }
-
-            Time.timeScale = 0;
+            
+            fadeIn = true;
         }
 
+        if (!fadeOut)
+        {
+            while (theEnd.GetComponent<Text>().color.a < 1)
+            {
+                Debug.Log("In text while loop");
+                //adjust fade amount by alpha directly compare to time not frames
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                Debug.Log("fade amount set");
+                //creates new color
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+
+                Debug.Log("Object Color set");
+                //and sets it to the black squares color
+                theEnd.GetComponent<Text>().color = objectColor;
+
+                Debug.Log("Text Color changed");
+                //halts everything until the while loop ends
+                yield return null;
+            }
+        }
     }
 }
