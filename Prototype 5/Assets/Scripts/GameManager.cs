4518a6c7b89ea1c7037190302a2e051f10360a51
mostlyghostly <alyssapeterson300@gmail.com>
2020-12-05 11:12:36 -0700

    Prototype 5: finished with commented code

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -7,64 +7,79 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //list of prefabs
     public List<GameObject> targets;
 
+    //how quickly those prefabs spawn
     private float spawnRate = 1.0f;
 
+    //Keeps track of player score
     private int score;
 
+    //Text and button for UI
     public TextMeshProUGUI scoreText;
 
     public TextMeshProUGUI gameOverText;
 
     public Button restartButton;
 
+    //control for the while loop
     public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        //initializes isgameactive bool
         isGameActive = true;
 
-        StartCoroutine(SpawnTarget());
+        //initializes score
         score = 0;
 
+        //initializes the score text
         scoreText.text = "Score: " + score;
+
+        //starts the coroutine to begin spawning objects
+        StartCoroutine(SpawnTarget());
+        
+        //sets game over screen to inactive
         gameOverText.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(false); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator SpawnTarget()
     {
+        //while the game is not over
         while(isGameActive)
         {
+            //wait for spawn
             yield return new WaitForSeconds(spawnRate);
+
+            //select a random prefab
             int index = Random.Range(0, targets.Count);
+
+            //then instantiate that prefab
             Instantiate(targets[index]);
         }
     }
 
+    // adds players score
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score; 
     }
 
+    //reloads the scene to restart the game
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    //activates the game over scene
     public void GameOver()
     {
+        isGameActive = false;
         gameOverText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
-        isGameActive = false;
     }
 }
--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -4,31 +4,39 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    //rigid body of the prefab
     private Rigidbody targetRb;
 
+    //floats to control the movement and spawn position of the prefab
     private float minSpeed = 12, 
                   maxSpeed = 16,
                   maxTorque = 10,
                   xRange = 4,
                   ySpawnPos = -6;
 
+    //gets the game manager script
     private GameManager gameManager;
 
+    //keeps track of the point value of the prefab
     public int pointValue;
 
+    //particle system
     public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
+        //initializes the physics
         targetRb = gameObject.GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
         transform.position = RandomSpawnPos();
 
+        //initializes the game manager script
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
+    //random functions to help initialize the physics
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
@@ -44,29 +52,31 @@ public class Target : MonoBehaviour
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
+    //when the mouse is clicked
     void OnMouseDown()
     {
+        //if the game is active
         if(gameManager.isGameActive)
         {
+            //destroy the item clicked
             Destroy(gameObject);
+            //set off the particles
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            //update the player's score
             gameManager.UpdateScore(pointValue);
         }
-    }
 
-    void OnTriggerEnter(Collider Other)
-    {
-        Destroy(gameObject);
-
-        if(!gameObject.CompareTag("bad"))
+        //if the object is bad
+        if(gameObject.CompareTag("Bad"))
         {
+            //trigger game over
             gameManager.GameOver();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //prevents prefabs from colliding with eachother
+    void OnTriggerEnter(Collider Other)
     {
-        
+        Destroy(gameObject);
     }
 }
